bf0143a3f97c33f222b3dce4250864cb320f8144
Sam Luen-English <sam.luenenglish@atheon.co.uk>
2018-04-23 15:26:50 +0100

    Initial commit

---
+++ dbio/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for DBio."""
+
+__author__ = """Samuel Luen-English"""
+__email__ = 'sam.luenenglish@atheon.co.uk'
+__version__ = '0.0.0'
---
+++ tests/test_dbio.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `dbio` package."""
+
+import pytest
+
+
+from dbio import dbio
+
